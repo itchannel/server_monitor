30708fe1660fde2a1849067917ac6a38fdbdaae2
Steve <steve@itchannel.me>
2020-12-30 17:40:37 +1000

    Added config and multi disk support

--- monitor.py
+++ monitor.py
@@ -2,21 +2,24 @@ import psutil
 import flask
 import json
 
-def diskstats(disk):
-	obj_Disk = psutil.disk_usage(disk)
-
-	total = obj_Disk.total / (1024.0 ** 3),"GB"
-	used = obj_Disk.used / (1024.0 ** 3),"GB"
-	free = obj_Disk.free / (1024.0 ** 3),"GB"
-	percent = obj_Disk.percent
-	return {"name":disk, "total":total, "used":used,"free":free,"percent":percent}
+def diskstats(disks):
+	diskret = []
+	for disk in disks:
+		obj_Disk = psutil.disk_usage(disk["location"])
+		
+		total = obj_Disk.total / (1024.0 ** 3),"GB"
+		used = obj_Disk.used / (1024.0 ** 3),"GB"
+		free = obj_Disk.free / (1024.0 ** 3),"GB"
+		percent = obj_Disk.percent
+		diskret.append( {"name":disk, "total":total, "used":used,"free":free,"percent":percent})
+	return diskret
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/stats', methods=["GET"])
 def stats():
-	test = diskstats('/')
+	test = diskstats(config["disks"])
 	data = { "disks":test}
 	
 	return json.dumps(data)
