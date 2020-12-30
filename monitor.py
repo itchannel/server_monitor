580df57e79e7e903398ff0228cb531e1e0382f55
Steve <steve@itchannel.me>
2020-12-30 20:02:40 +1000

    Added windows poweroff

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
 import json
 import psutil
 import flask
+import os
 
 
 # Process Disk Stats for given mount points
@@ -21,19 +22,30 @@ def diskstats(disks):
     return diskret
 
 
+# Get CPU Status
+def cpustats():
+    print("Coming Soon")
+
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/stats', methods=["GET"])
 def stats():
-    test = diskstats(config["disks"])
-    data = {"disks": test}
+    disks = diskstats(config["disks"])
+    data = {"disks": disks}
     return json.dumps(data)
+
+
+@app.route('/suspend', methods=["GET"])
+def suspend():
+    if os.name == "nt":
+        os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
 
 
 with open('monitor.conf', 'r') as f:
     config = json.load(f)
 
-
+print(os.name)
 app.run(host="0.0.0.0", port=5001)
